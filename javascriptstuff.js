3a71e05f7c2e8cfa05929eb918ede62b7f1eb421
Clink123 <jamie.lepone@gmail.com>
2016-12-11 14:15:14 -0500

    landing page V1.1 & adbout page V1.0

---
+++ javascriptstuff.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.slider').slider({
+        full_width: false
+        , indicators: false
+        , interval: 3000
+    });
+    $(".button-collapse").sideNav();
+});
